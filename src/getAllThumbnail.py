878428b02943d8ac10aa135a95dc27e4346527c5
Jun Jiang <smujiang@gmail.com>
2019-04-19 15:46:57 -0500

    Create getAllThumbnail.py

---
+++ src/getAllThumbnail.py
@@ -0,0 +1,35 @@
+'''
+Extract image patches from WSIs and encode the non-blank patches into npy files.
+'''
+
+
+import openslide
+import os
+import fnmatch
+data_dir = "//mfad.mfroot.org/researchmn/DLMP-MACHINE-LEARNING/Mitosis_Deep Learning"
+data_out_dir = "H://HE_IHC_Stains/thumbnails"
+level = 0
+thumbnail_max_size = (512,512)
+
+if __name__ == '__main__':
+    WSI_names = fnmatch.filter(os.listdir(data_dir), 'Biliary*')
+    for wsi_name in WSI_names:
+        slide_path = os.path.join(data_dir, wsi_name)
+        # out_path = os.path.join(data_out_dir, wsi_name[0:-4])
+        if not os.path.exists(data_out_dir):
+            os.makedirs(data_out_dir)
+        WSI = openslide.OpenSlide(slide_path)
+        thumbnail = WSI.get_thumbnail(thumbnail_max_size)
+        thumbnail.save(os.path.join(data_out_dir, wsi_name[0:-4] + ".jpg"))
+
+
+
+
+
+
+
+
+
+
+
+
